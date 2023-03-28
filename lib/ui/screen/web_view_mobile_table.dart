4e9741f5e17cff6f644a2a047a13bcca8bfa21fe
Alireza Bashi <alirezabashi98@gmail.com>
2023-03-28 14:13:52 +0330

    const SizedBox(width: 20),

--- lib/ui/screen/web_view_mobile_table.dart
+++ lib/ui/screen/web_view_mobile_table.dart
@@ -15,9 +15,26 @@ class _WebViewMobileAndTabletState extends State<WebViewMobileAndTablet> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Alfa Coin (${widget.cryptoSymbol})'),
+        elevation: 0,
+        actions: [
+          GestureDetector(
+            onTap: () => webviewController.goBack(),
+            child: const Icon(Icons.arrow_back_rounded),
+          ),
+           const SizedBox(width: 20),
+          GestureDetector(
+            onTap: () => webviewController.goForward(),
+            child: const Icon(Icons.arrow_forward_rounded),
+          ),
+           const SizedBox(width: 20),
+        ],
+      ),
       body: SafeArea(
         child: WebViewX(
-          initialContent: 'https://www.tradingview.com/chart/?symbol=BITSTAMP%3A${widget.cryptoSymbol}USD',
+          initialContent:
+              'https://www.tradingview.com/chart/?symbol=BITSTAMP%3A${widget.cryptoSymbol}USD',
           initialSourceType: SourceType.url,
           onWebViewCreated: (controller) => webviewController = controller,
           height: double.infinity,
